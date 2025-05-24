0a15e16a35a923e81d1d1ef9521137056f746bca
archiquito <archiquito@gmail.com>
2025-05-24 15:57:29 -0300

    adjustments to run

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,26 +1,27 @@
-import { Roboto } from 'next/font/google';
-import './globals.css';
-import { Header } from '@/components/Header';
-import { Footer } from '@/components/Footer';
+import { Roboto } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 const roboto = Roboto({
-  variable: '--font-roboto',
-  subsets: ['latin'],
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
 });
 
 export const metadata = {
-  title: 'Teste Cogna',
-  description: 'Teste Cogna - 2025',
-  keywords: ['Next.js', 'React', 'JavaScript'],
-  publisher: 'Cogna',
-  creator: 'André Chiquito',
+  title: "Teste Cogna",
+  description: "Teste Cogna - 2025",
+  keywords: ["Next.js", "React", "JavaScript"],
+  publisher: "Cogna",
+  creator: "André Chiquito",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${roboto.variable} ${roboto.variable} antialiased min-h-screen flex flex-col`}
+        className={`${roboto.variable} antialiased min-h-screen flex flex-col`}
       >
         <Header />
         <main className="flex-1 container mx-auto">{children}</main>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { formatPrice } from '@/utils/formatPrice';
-import { replaceWhiteSpaceToUnderscore } from '@/utils/replaceWhiteSpaceToUnderscore';
-import { ShoppingCart } from 'lucide-react';
+import Link from "next/link";
+import Image from "next/image";
+import { formatPrice } from "@/utils/formatPrice";
+import { replaceWhiteSpaceToUnderscore } from "@/utils/replaceWhiteSpaceToUnderscore";
+import { ShoppingCart } from "lucide-react";
 
 import {
-  CardContent, CardFooter, CardHeader, CardTitle,
-} from './ui/card';
-import { Button } from './ui/button';
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Button } from "./ui/button";
 
 export function Products({ data }) {
   return (
@@ -19,40 +23,40 @@ export function Products({ data }) {
     >
       {data?.map((item) => (
         <article key={item.id} className="w-[350px] border-accent border-2">
-          <CardHeader>
-            <CardContent className="grid-cols-2 gap-2.5 flex items-center justify-center">
-              <div className="flex items-center justify-center h-[150px]">
-                <Image
-                  src={item.image}
-                  alt={item.title}
-                  quality={75}
-                  width={150}
-                  height={150}
-                  className="flex items-center justify-center object-contain max-h-[150px]"
-                />
-              </div>
-            </CardContent>
-            <CardTitle className="font-bold text-sm h-16">
-              {item.title}
-            </CardTitle>
-            <p className="font-bold text-2xl">{formatPrice(item.price)}</p>
-          </CardHeader>
-          <CardFooter className="flex flex-col items-center justify-end flex-wrap">
-            <Button
-              asChild
-              className="w-full bg-emerald-600 hover:bg-emerald-700"
-            >
-              <Link
-                href={`/product/${item.id}/${replaceWhiteSpaceToUnderscore(
-                  item.title,
-                )}`}
+          <Card>
+            <CardHeader>
+              <CardContent className="grid-cols-2 gap-2.5 flex items-center justify-center">
+                <div className="flex items-center justify-center h-[150px]">
+                  <Image
+                    src={item.image}
+                    alt={item.title}
+                    quality={75}
+                    width={150}
+                    height={150}
+                    className="flex items-center justify-center object-contain max-h-[150px]"
+                  />
+                </div>
+              </CardContent>
+              <CardTitle className="font-bold text-sm h-16">
+                {item.title}
+              </CardTitle>
+              <p className="font-bold text-2xl">{formatPrice(item.price)}</p>
+            </CardHeader>
+            <CardFooter className="flex flex-col items-center justify-end flex-wrap">
+              <Button
+                asChild
+                className="w-full bg-emerald-600 hover:bg-emerald-700"
               >
-                <ShoppingCart />
-                {' '}
-                Ver Produto
-              </Link>
-            </Button>
-          </CardFooter>
+                <Link
+                  href={`/product/${item.id}/${replaceWhiteSpaceToUnderscore(
+                    item.title
+                  )}`}
+                >
+                  <ShoppingCart /> Ver Produto
+                </Link>
+              </Button>
+            </CardFooter>
+          </Card>
         </article>
       ))}
     </div>
